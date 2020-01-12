e417c36b6d2575344edc6c859f75d5ea5d58234b
Shane Osbourne <shane.osbourne8@gmail.com>
2020-01-12 19:34:58 +0000

    rename attempt

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,4 @@ export * from './RxRadioGroup';
 export * from './RxText';
 export * from './RxSelect';
 export * from './RxTextArea';
-export { dropKey } from './utils/general';
-export { hasValue } from './utils/general';
 export { RxFormContext } from './Context';
